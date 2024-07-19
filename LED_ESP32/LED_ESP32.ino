2297dfb420a8cb25836755b17934adb778a1f37d
GarciadeLeonJuan <99160462+GarciadeLeonJuan@users.noreply.github.com>
2024-07-18 22:48:10 -0600

    led_push_button

---
+++ LED_ESP32/LED_ESP32.ino
@@ -0,0 +1,28 @@
+#define LED_PIN 23 
+#define BUTTON_PIN 4 
+
+int buttonState = 0; 
+int lastButtonState = 0; 
+bool ledState = false; 
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); 
+}
+
+void loop() {
+  // Leer el estado del botón
+  buttonState = digitalRead(BUTTON_PIN);
+
+
+  if (buttonState != lastButtonState) {
+
+    if (buttonState == LOW) {
+      ledState = !ledState; 
+      digitalWrite(LED_PIN, ledState); 
+    }
+    delay(50); 
+  }
+
+  lastButtonState = buttonState; 
+}
